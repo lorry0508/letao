92e7c6decf5435e9fa5047d5adcbfa9d846a7b03
lorry0508 <1165354824@qq.com>
2019-07-14 09:17:03 +0800

    register

---
+++ public/m/js/public.js
@@ -0,0 +1,9 @@
+//mui默认禁止了a标签的跳转，所以要在全局恢复a标签的跳转
+$(function(){
+    //恢复a元素的跳转,事件委托
+    $("body").on("tap","a",function(){
+        mui.openWindow({
+            url:$(this).attr(href)
+        })
+    })
+})
